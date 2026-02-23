633e0c69b756b9346cdd3d5ac015a5b58ac752f1
LordBaryhobal <lordbaryhobal@gmail.com>
2026-02-23 11:35:34 +0100

    feat(tracing): define pipeline orchestration
    Co-authored-by: jijiduc <jijiduc23@gmail.com>

--- tracing/tracer.py
+++ tracing/tracer.py
@@ -1,7 +1,9 @@
 from pathlib import Path
+from typing import Optional
 
 import numpy as np
 from PIL.Image import Image
+import tqdm
 
 from tracing.color import Color
 from tracing.island import Island
@@ -11,31 +13,58 @@ from tracing.trace import Trace
 
 
 class Tracer:
-    def __init__(self, texture_path: Path, model_path: Path, palette: tuple[Color, ...]):
+    def __init__(
+        self, texture_path: Path, model_path: Path, palette: tuple[Color, ...]
+    ):
         self.texture_path: Path = texture_path
         self.model_path: Path = model_path
         self.palette: tuple[Color, ...] = palette
+
+        self.texture: Optional[Image] = None
+        self.layers: list[Image] = []
 
         self.islands: list[Island] = []
         self.segments: list[Segment] = []
         self.traces: list[Trace] = []
 
     def compute_traces(self) -> list[Trace]:
+        self.texture = self.load_texture(self.texture_path)
+        self.texture = self.discretize_texture_colors(self.texture)
+        self.layers = self.split_colors(self.texture)
+
+        for c, layer in tqdm.tqdm(
+            enumerate(self.layers), desc="Island detection", unit="layer"
+        ):
+            islands: list[Island] = self.detect_islands(layer, c)
+            self.islands.extend(islands)
+
+        for island in tqdm.tqdm(
+            self.islands, desc="Island segmentation", unit="island"
+        ):
+            border: list[Segment] = self.resample_border(island)
+            self.segments.extend(border)
+            fill_slices: list[Segment] = self.compute_fill_slices(island)
+            self.segments.extend(fill_slices)
+
+        for segment in tqdm.tqdm(self.segments, desc="3D projection", unit="segment"):
+            trace: Trace = self.project_segment_to_3d(segment)
+            self.traces.append(trace)
+
         return self.traces
 
-    def load_texture(self):
+    def load_texture(self, path: Path) -> Image:
         pass
 
-    def load_model(self):
+    def load_model(self, path: Path) -> Mesh:
         pass
 
-    def discretize_texture_colors(self):
+    def discretize_texture_colors(self, img: Image) -> Image:
         pass
 
     def split_colors(self, img: Image) -> list[Image]:
         return []
 
-    def detect_islands(self, img: Image) -> list[Island]:
+    def detect_islands(self, img: Image, color: int) -> list[Island]:
         return []
 
     def resample_border(self, island: Island) -> list[Segment]:
