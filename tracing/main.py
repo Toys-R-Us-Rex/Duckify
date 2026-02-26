397c8388a741a8cdb270a25507ba56f66240d67d
LordBaryhobal <lordbaryhobal@gmail.com>
2026-02-27 00:06:45 +0100

    feat(tracing): add multicolor texture example and display colors in debug view

--- tracing/main.py
+++ tracing/main.py
@@ -9,14 +9,14 @@ OUTPUT_DIR = PROJECT_DIR / "output"
 
 
 def main():
-    texture_path: Path = ASSETS_DIR / "textures" / "trapezoid_letters.png"
+    texture_path: Path = ASSETS_DIR / "textures" / "trapezoid_colors.png"
     model_path: Path = ASSETS_DIR / "models" / "trapezoid_letters.obj"
     texture_name: str = texture_path.stem
     model_name: str = model_path.stem
     output_path: Path = OUTPUT_DIR / f"{model_name}-{texture_name}-trace.json"
-    palette: tuple[Color, ...] = ((255, 255, 255),)
+    palette: tuple[Color, ...] = ((255, 255, 255), (255, 0, 0), (0, 255, 0))
 
-    tracer: Tracer = Tracer(texture_path, model_path, palette, output_path)
+    tracer: Tracer = Tracer(texture_path, model_path, palette, output_path, debug=True)
     tracer.compute_traces()
 
 
