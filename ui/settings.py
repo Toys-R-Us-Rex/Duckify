06fde82e7b98d4c0e7388bd9364d7280d9850435
LordBaryhobal <lordbaryhobal@gmail.com>
2026-03-16 19:01:11 +0100

    feat(ui): implement gen ai test connection button

--- ui/settings.py
+++ ui/settings.py
@@ -1,3 +1,5 @@
+import urllib.request
+
 from PyQt6 import QtWidgets
 from settings_manager import GenAISettings, Settings, TracingSettings
 from settings_ui import Ui_Dialog
@@ -8,6 +10,8 @@ class SettingsDialog(QtWidgets.QDialog, Ui_Dialog):
         super().__init__(parent)
         self.setupUi(self)
         self.set_settings(current_settings)
+
+        self.genAITest.clicked.connect(self.test_genai_connection)
 
     def get_settings(self) -> Settings:
         return Settings(
@@ -30,3 +34,15 @@ class SettingsDialog(QtWidgets.QDialog, Ui_Dialog):
     def set_palette(self, palette: list[tuple[int, int, int]]):
         # TODO
         pass
+
+    def test_genai_connection(self):
+        host: str = self.genAIHost.text()
+        port: int = self.genAIPort.value()
+        try:
+            urllib.request.urlopen(f"http://{host}:{port}").read()
+            self.set_genai_connection_status("Connection successful")
+        except:
+            self.set_genai_connection_status("Connection failed")
+
+    def set_genai_connection_status(self, status: str):
+        self.genAITestResult.setText(status)
