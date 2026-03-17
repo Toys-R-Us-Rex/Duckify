a4f3c5aa4f37adb20045a63bef62945bc4d4fde4
LordBaryhobal <lordbaryhobal@gmail.com>
2026-03-17 10:59:13 +0100

    chore(ui): add build-ui script

--- ui/build_ui_files.py
+++ ui/build_ui_files.py
@@ -5,8 +5,9 @@ from pathlib import Path
 def build_ui():
     print("Building UI files...")
     for ui_file in Path(__file__).parent.glob("*.ui"):
-        py_file = f"{ui_file.stem}_ui.py"
-        subprocess.run(["pyuic6", str(ui_file), "-o", py_file], check=True)
+        py_file = ui_file.parent / f"{ui_file.stem}_ui.py"
+        print(f"{ui_file} -> {py_file}")
+        subprocess.run(["pyuic6", str(ui_file), "-o", str(py_file)], check=True)
 
 
 if __name__ == "__main__":
