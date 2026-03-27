678dc9f771053826af8aa37b45e2176aa7f4ca35
jijiduc <jijiduc23@gmail.com>
2026-03-27 14:47:56 +0100

    chore(tracing): debug visualization not shown by default

--- tracing/config.py
+++ tracing/config.py
@@ -41,14 +41,14 @@ class TracerConfig:
     parallel_angle: float = np.radians(1)
     """Angle radius threshold when considering too parallel faces"""
 
-    enable_reduction_visualisation: bool = True
+    enable_reduction_visualisation: bool = False
     """Whether reduction's visualisations should be displayed"""
 
-    enable_inputs_visualisation: bool = True
+    enable_inputs_visualisation: bool = False
     """Whether input's visualisations should be displayed"""
 
-    enable_texture_transformation_visualisation: bool = True
+    enable_texture_transformation_visualisation: bool = False
     """Whether texture transformation's (mask, palettization, color split) visualisations should be displayed"""
 
-    enable_island_selection_visualisation: bool = True
+    enable_island_selection_visualisation: bool = False
     """Whether island selsection's visualisations should be displayed"""
