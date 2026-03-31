6d5259fbfcd578398940125063ad4d8ca547b025
LordBaryhobal <lordbaryhobal@gmail.com>
2026-03-31 17:56:05 +0200

    feat(ui): implement open assets dir action

--- ui/assets.py
+++ ui/assets.py
@@ -4,12 +4,13 @@ from pathlib import Path
 class AssetRegistry:
     def __init__(self, root_dir: Path) -> None:
         self.root_dir: Path = root_dir
-        self.models_dir: Path = self.root_dir / "assets" / "models"
-        self.textures_dir: Path = self.root_dir / "assets" / "textures"
+        self.assets_dir: Path = self.root_dir / "assets"
+        self.models_dir: Path = self.assets_dir / "models"
+        self.textures_dir: Path = self.assets_dir / "textures"
         self.masks_dir: Path = self.textures_dir / "masks"
         self.output_dir: Path = self.root_dir / "output"
         self.transformation_reference: Path = (
-            self.root_dir / "assets" / "transformation_reference.json"
+            self.assets_dir / "transformation_reference.json"
         )
         self.default_tcp_calibration_path: Path = self.root_dir / "robot" / "duckify_simulation" / "defaults" / "calibration_default.pkl"
         self.test_transformation_path: Path = self.root_dir / "robot" / "duckify_simulation" / "defaults" / "transformation_test.pkl"
